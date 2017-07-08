2e16998833e7a903fadef9b7f30690f359463329
Andrea Mihaljević <andrea.mihaljevic@hotmail.com>
2017-07-08 18:58:35 +0200

    Add files via upload

---
+++ mobilna/www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('starter.controllers', [])
+.controller ('LogoutCtrl', function($scope) {})
+
+.controller ('LoginCtrl', function($scope) {})
+
+.controller ('ZadaceCtrl', function($scope) {})
+
